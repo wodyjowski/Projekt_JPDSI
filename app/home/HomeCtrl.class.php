96a0834ac7caa28f29d7d7eff5aa829dc468e1af
Michał Kisiel <mkisiel58@hotmail.com>
2017-05-17 00:50:52 +0200

    Fixed missing Jquery and javascript in Bootstrap
    Added loading posts from database

--- app/home/HomeCtrl.class.php
+++ app/home/HomeCtrl.class.php
@@ -2,16 +2,24 @@
 
 class HomeCtrl{
 
+    private $posts;
 
 
     public function process(){
 
+        $where = "";
+
+        $this->posts = getDB()->select("post", [
+            "title",
+            "content"
+        ], $where );
 
 
         $this->generateView();
     }
 
     public function generateView(){
+        getSmarty()->assign("posts",$this->posts);
         getSmarty()->display(getConf()->root_path.'/app/home/Home.html');
     }
 }
---
+++ app/home/PostData.class.php
@@ -0,0 +1,4 @@
+<?php
+class PostData {
+	public $surname;
+} 
